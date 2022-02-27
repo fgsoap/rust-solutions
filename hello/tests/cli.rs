96786201ebcf0ea22b847a671034d5d092f22fe9
Scott Xu <fgsoap@gmail.com>
2022-02-27 17:16:13 +0800

    adding assert_cmd

--- hello/tests/cli.rs
+++ hello/tests/cli.rs
@@ -1,7 +1,6 @@
-use std::process::Command;
+use assert_cmd::Command;
 #[test]
 fn runs() {
-    let mut cmd = Command::new("ls");
-    let res = cmd.output();
-    assert!(res.is_ok());
+    let mut cmd = Command::cargo_bin("hello").unwrap();
+    cmd.assert().success();
 }
