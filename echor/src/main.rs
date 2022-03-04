44dc5f51c473f2c4355472fb677a6e126dde67a2
Scott Xu <fgsoap@gmail.com>
2022-03-04 23:05:37 +0800

    first version with clap for echor

--- echor/src/main.rs
+++ echor/src/main.rs
@@ -1,3 +1,23 @@
+use clap::{Arg, Command};
 fn main() {
-    println!("{:?}", std::env::args());
+    // println!("{:?}", std::env::args());
+    let matchs = Command::new("echor")
+        .version("0.1.0")
+        .author("Node JX <fgsoap@gmail.com>")
+        .about("Rust echo")
+        .arg(
+            Arg::new("text")
+                .value_name("TEXT")
+                .help("Input text")
+                .required(true)
+                .min_values(1),
+        )
+        .arg(
+            Arg::new("omit_newline")
+                .short('n')
+                .help("Do not print newline")
+                .takes_value(false),
+        )
+        .get_matches();
+    println!("{:#?}", matchs);
 }
