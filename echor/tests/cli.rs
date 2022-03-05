390a9cebd311af09d4faa1b8cb075670b4c9ca70
Scott Xu <fgsoap@gmail.com>
2022-03-05 21:13:22 +0800

    adding TestResult to tests of echor

--- echor/tests/cli.rs
+++ echor/tests/cli.rs
@@ -3,24 +3,29 @@ use std::fs;
 use assert_cmd::Command;
 use predicates::prelude::*;
 
+type TestResult = Result<(), Box<dyn std::error::Error>>;
+
 #[test]
-fn dies_no_args() {
-    let mut cmd = Command::cargo_bin("echor").unwrap();
+fn dies_no_args() -> TestResult {
+    let mut cmd = Command::cargo_bin("echor")?;
     cmd.assert()
         .failure()
         .stderr(predicate::str::contains("USAGE"));
+    Ok(())
 }
 
 #[test]
-fn runs() {
-    let mut cmd = Command::cargo_bin("echor").unwrap();
+fn runs() -> TestResult {
+    let mut cmd = Command::cargo_bin("echor")?;
     cmd.arg("foo").assert().success();
+    Ok(())
 }
 
 #[test]
-fn hello() {
+fn hello() -> TestResult {
     let outfile = "tests/expected/hello1.txt";
-    let expected = fs::read_to_string(outfile).unwrap();
-    let mut cmd = Command::cargo_bin("echor").unwrap();
+    let expected = fs::read_to_string(outfile)?;
+    let mut cmd = Command::cargo_bin("echor")?;
     cmd.arg("Hello there").assert().success().stdout(expected);
+    Ok(())
 }
