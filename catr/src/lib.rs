a192850a69ac8287002f7ac04251887197c11b33
Scott Xu <fgsoap@gmail.com>
2022-03-12 15:01:47 +0800

    adding get_args to catr

--- catr/src/lib.rs
+++ catr/src/lib.rs
@@ -1,8 +1,54 @@
+use serde::{Deserialize, Serialize};
 use std::error::Error;
+
+use clap::{Arg, Command};
+
+#[derive(Debug, Deserialize, Serialize)]
+pub struct Config {
+    files: Vec<String>,
+    number_lines: bool,
+    number_nonblank_lines: bool,
+}
 
 type MyResult<T> = Result<T, Box<dyn Error>>;
 
-pub fn run() -> MyResult<()> {
-    println!("Hello, world!");
+pub fn run(config: Config) -> MyResult<()> {
+    // println!("Hello, world!");
+    dbg!(config);
     Ok(())
+}
+
+pub fn get_args() -> MyResult<Config> {
+    let matches = Command::new("catr")
+        .version("0.1.0")
+        .author("Node JX <fgsoap@gmail.com>")
+        .about("Rust cat")
+        .arg(
+            Arg::new("files")
+                .value_name("FILES")
+                .help("Input files")
+                .multiple_values(true)
+                .default_value("-"),
+        )
+        .arg(
+            Arg::new("number")
+                .short('n')
+                .long("number")
+                .help("Number lines")
+                .takes_value(false)
+                .conflicts_with("number_nonblank"),
+        )
+        .arg(
+            Arg::new("number_nonblank")
+                .short('b')
+                .long("number-nonblank")
+                .help("Number non-blank lines")
+                .takes_value(false),
+        )
+        .get_matches();
+    Ok(Config {
+        files: matches.values_of_lossy("files").unwrap(),
+        number_lines: matches.is_present("number"),
+        number_nonblank_lines: matches.is_present("number_nonblank"),
+    })
 }
