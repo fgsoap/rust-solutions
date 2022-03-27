413d759231edd9e7c52f38066d3346dff2d8ada1
Scott Xu <fgsoap@gmail.com>
2022-03-27 16:20:33 +0800

    adding config-rs

--- reverse-proxy/src/main.rs
+++ reverse-proxy/src/main.rs
@@ -1,10 +1,27 @@
+use std::collections::HashMap;
+
 use chrono::Utc;
+use config::Config;
 use serde_json;
 use warp::{http::HeaderValue, hyper::HeaderMap, Filter};
 use warp_reverse_proxy::reverse_proxy_filter;
 
 #[tokio::main]
 async fn main() {
+    let settings = Config::builder()
+        // Add in `./Settings.toml`
+        .add_source(config::File::with_name("Settings.toml").required(true))
+        // Add in settings from the environment (with a prefix of APP)
+        // Eg.. `APP_DEBUG=1 ./target/app` would set the `debug` key
+        .add_source(config::Environment::with_prefix("APP"))
+        .build()
+        .unwrap();
+
+    let s = settings
+        .try_deserialize::<HashMap<String, String>>()
+        .unwrap();
+    let upstream = s.get("upstream").unwrap();
+
     let log = warp::log::custom(|info| {
         // Use a log macro, or slog, or println, or whatever!
         eprintln!(
@@ -26,7 +43,7 @@ async fn main() {
 
     // Forward request to localhost in other port
     let app = warp::path!("echo" / ..).and(
-        reverse_proxy_filter("".to_string(), "https://reqbin.com/".to_string())
+        reverse_proxy_filter("".to_string(), upstream.to_string())
             // .and_then(log_response)
             .with(log),
     );
