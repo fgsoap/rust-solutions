6e2c60f7c140f0f6a6107c5bdbfdebe11f723181
Scott Xu <fgsoap@gmail.com>
2023-03-30 21:57:15 +0800

    add Result

--- minigrep/src/main.rs
+++ minigrep/src/main.rs
@@ -1,21 +1,36 @@
 use std::env;
 use std::fs;
+use std::process;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
-    let (query, file_path) = parse_config(&args);
+    let config = Config::build(&args).unwrap_or_else(|err| {
+        println!("Problem parsing arguments: {err}");
+        process::exit(1)
+    });
 
-    println!("Searching for {}", query);
-    println!("In file {}", file_path);
+    println!("Searching for {}", config.query);
+    println!("In file {}", config.file_path);
 
-    let contents = fs::read_to_string(file_path).expect("Should have been able to read the file");
+    let contents =
+        fs::read_to_string(config.file_path).expect("Should have been able to read the file");
 
     println!("With text:\n{contents}");
 }
 
-fn parse_config(args: &[String]) -> (&str, &str) {
-    let query = &args[1];
-    let file_path = &args[2];
+struct Config {
+    query: String,
+    file_path: String,
+}
 
-    (query, file_path)
+impl Config {
+    fn build(args: &[String]) -> Result<Config, &'static str> {
+        let query = &args[1];
+        let file_path = &args[2];
+
+        Ok(Config {
+            query: query.to_string(),
+            file_path: file_path.to_string(),
+        })
+    }
 }
